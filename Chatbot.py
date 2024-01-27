e1f3b4f10261c9c3c70e3116d6e61c0cc86c31d3
omphcompany <126784041+omphcompany@users.noreply.github.com>
2024-01-27 05:36:29 -0700

    Update Chatbot.py
    Parameters

--- Chatbot.py
+++ Chatbot.py
@@ -1,21 +1,14 @@
 import streamlit as st
 
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import initialize_agent, AgentType  
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("🔎 Chat with Open AI GPT-3.5")
-
-"""
-An Internet Enabled Streamlit.
-"""
+st.title("🔎 Chat with OpenAI GPT-3.5")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
@@ -24,18 +17,26 @@ if "messages" not in st.session_state:
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-
+    
 if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
+    
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", openai_api_key=openai_api_key, streaming=True)
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo-1106", 
+        openai_api_key=openai_api_key, 
+        streaming=True,
+        temperature=0.3,
+        top_k=40,
+        top_p=1,
+        max_tokens=800
+    )
+    
     search = DuckDuckGoSearchRun(name="Search")
     search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
+    
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
         response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
